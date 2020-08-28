da958d21345810609ff25adb32bcc111e770a03c
FilBil <63282652+FilBil@users.noreply.github.com>
2020-08-28 11:43:16 +0200

    New Models

---
+++ Models/TourUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventMeeting.Models
+{
+    public class TourUser
+    {
+        public int ID { get; set; }
+        public String Login { get; set; }
+        public String Password { get; set; }
+        public String UserEmail { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        
+    }
+}
