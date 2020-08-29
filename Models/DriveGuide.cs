1e2877cb43d2ddc92d76ff7216ad14440708bcc7
FilBil <63282652+FilBil@users.noreply.github.com>
2020-08-29 16:43:40 +0200

    New Models

---
+++ Models/DriveGuide.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventMeeting.Models
+{
+    public class DriveGuide
+    {
+        public int ID { get; set; }
+        public String Login { get; set; }
+        public String Password { get; set; }
+        public String UserEmail { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public bool IsBusy { get; set; }
+        public decimal DrivePrice { get; set; }
+    }
+}
--- Models/TourGuide.cs
+++ Models/TourGuide.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace EventMeeting.Models
 {
-    public class TourUser
+    public class TourGuide
     {
         public int ID { get; set; }
         public String Login { get; set; }
@@ -13,6 +13,8 @@ namespace EventMeeting.Models
         public String UserEmail { get; set; }
         public String Name { get; set; }
         public String Surname { get; set; }
+        public bool IsBusy { get; set; }
+        public decimal TourPrice { get; set; }
         
     }
 }
