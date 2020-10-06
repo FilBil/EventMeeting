d53b3da5a63a9bf74dccfb2956de27cda441c29a
OskarPrzydatek <o.przydatek@wp.pl>
2020-10-06 19:52:02 +0200

    app.module

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlatsForRentComponent } from './functionalities/flats-for-rent/flats-for-rent/flats-for-rent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlatsForRentComponent
   ],
   imports: [
     BrowserModule,
